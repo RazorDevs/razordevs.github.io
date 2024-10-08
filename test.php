92701a4e2139c9b34b47ea4c37bb382a0da7d585
darksonic300 <fabiross096@gmail.com>
2024-10-08 09:09:07 +0200

    PHP commit

---
+++ test.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <header>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    </header>
+    <body>
+        <h1>Testing a PHP page</h1>
+        <p>With the use of a CSS Stylesheet and Bootstrap.</p>
+    </body>
+</html>
+
+<?php
+
+?>
